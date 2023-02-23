00c7e0a7c75df0e574b1c3dc98a94362c7bcbfba
pawelstankiewicz <pawelsta95@gmail.com>
2023-02-23 23:36:10 +0100

    feat: add Builder annotation for ProductCategory class

--- src/main/java/com/pawelapps/ecommerce/entity/ProductCategory.java
+++ src/main/java/com/pawelapps/ecommerce/entity/ProductCategory.java
@@ -14,6 +14,7 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Builder
 public class ProductCategory implements Serializable {
 
     @Id
