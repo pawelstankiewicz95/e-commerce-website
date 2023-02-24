8680a50241208ad809b00d5e7b3d6ed6bfdf7657
pawelstankiewicz <pawelsta95@gmail.com>
2023-02-24 23:39:54 +0100

    test: fix setUpDataBase method

--- src/test/java/com/pawelapps/ecommerce/ProductRepositoryTest.java
+++ src/test/java/com/pawelapps/ecommerce/ProductRepositoryTest.java
@@ -3,6 +3,7 @@ package com.pawelapps.ecommerce;
 import com.pawelapps.ecommerce.dao.ProductRepository;
 import com.pawelapps.ecommerce.entity.Product;
 import com.pawelapps.ecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.test.context.TestPropertySource;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -19,12 +21,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProductRepositoryTest {
 
     @Autowired
+    EntityManager entityManager;
+    @Autowired
     ProductRepository productRepository;
     ProductCategory productCategory;
     Product product;
 
+
     @BeforeEach
-    void setUpDataBase(){
+    void setUpDataBase() {
         productCategory = ProductCategory.builder().categoryName("Book").build();
         product = Product.builder()
                 .sku("123456")
@@ -38,10 +43,12 @@ class ProductRepositoryTest {
                 .lastUpdated(LocalDateTime.now())
                 .productCategory(productCategory)
                 .build();
+        entityManager.persist(product);
+
     }
 
     @Test
-    void saveProductTest(){
+    void saveProductTest() {
         productRepository.save(product);
         assertTrue(product.getId() > 0, "should be greater than 0");
     }
