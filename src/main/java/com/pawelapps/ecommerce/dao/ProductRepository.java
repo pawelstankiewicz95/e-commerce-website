12bd057c21a7309a02b3ec668dbeca9a7239466e
pawelstankiewicz <pawelsta95@gmail.com>
2023-02-26 00:08:29 +0100

    feat: add findByName method

--- src/main/java/com/pawelapps/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/pawelapps/ecommerce/dao/ProductRepository.java
@@ -3,5 +3,8 @@ package com.pawelapps.ecommerce.dao;
 import com.pawelapps.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product,Long> {
+    List<Product> findByName(String name);
 }
