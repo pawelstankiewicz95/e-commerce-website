11428198fcb9e09b5de6647b2005a0d8bbb93893
pawelstankiewicz <pawelsta95@gmail.com>
2023-02-26 17:15:29 +0100

    test: setting up database for testing

--- src/test/java/com/pawelapps/ecommerce/dao/ProductCategoryRepositoryTest.java
+++ src/test/java/com/pawelapps/ecommerce/dao/ProductCategoryRepositoryTest.java
@@ -1,11 +1,48 @@
 package com.pawelapps.ecommerce.dao;
 
+import com.pawelapps.ecommerce.entity.Product;
+import com.pawelapps.ecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @DataJpaTest
 @TestPropertySource("/test-application.properties")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class ProductCategoryRepositoryTest {
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Autowired
+    ProductCategoryRepository productCategoryRepository;
+
+    Product product;
+    ProductCategory productCategory;
+
+    @BeforeEach
+    void setUpDataBase() {
+        product = Product.builder()
+                .sku("123456")
+                .name("TestCup")
+                .description("Just testing cup")
+                .unitPrice(BigDecimal.valueOf(24.35))
+                .imageUrl("www.test.com")
+                .active(true)
+                .unitsInStock(10)
+                .dateCreated(LocalDateTime.now())
+                .lastUpdated(LocalDateTime.now())
+                .productCategory(productCategory)
+                .build();
+        productCategory = ProductCategory.builder().categoryName("Cup").build();
+        productCategory.addProduct(product);
+        entityManager.persist(product);
+        entityManager.flush();
+    }
 }
