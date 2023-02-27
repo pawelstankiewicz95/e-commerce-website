68dc20eda2efee9800f6c7f509113e2c8e1d106d
pawelstankiewicz <pawelsta95@gmail.com>
2023-02-28 00:33:51 +0100

    feat: add createProduct method and necessary annotations

--- src/main/java/com/pawelapps/ecommerce/service/ProductServiceImpl.java
+++ src/main/java/com/pawelapps/ecommerce/service/ProductServiceImpl.java
@@ -1,4 +1,25 @@
 package com.pawelapps.ecommerce.service;
 
-public class ProductServiceImpl {
+import com.pawelapps.ecommerce.dao.ProductRepository;
+import com.pawelapps.ecommerce.entity.Product;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class ProductServiceImpl implements ProductService{
+
+    ProductRepository productRepository;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Product createProduct(Product product) {
+        productRepository.save(product);
+        return product;
+    }
 }
