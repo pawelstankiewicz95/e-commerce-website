977314d0547f2c7471fcaa22bbaa1d5e0d9432f5
pawelstankiewicz <pawelsta95@gmail.com>
2023-02-27 23:29:32 +0100

    test: add setUp method

---
+++ src/test/java/com/pawelapps/ecommerce/service/ProductServiceTest.java
@@ -0,0 +1,50 @@
+package com.pawelapps.ecommerce.service;
+
+import com.pawelapps.ecommerce.dao.ProductRepository;
+import com.pawelapps.ecommerce.entity.Product;
+import com.pawelapps.ecommerce.entity.ProductCategory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class ProductServiceTest {
+
+    @MockBean
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ProductService productService;
+
+    Product product;
+
+    ProductCategory productCategory;
+
+    @BeforeEach
+    void setUp() {
+        productCategory = ProductCategory.builder().categoryName("Cup").build();
+        product = Product.builder()
+                .sku("123456")
+                .name("TestCup")
+                .description("Just testing cup")
+                .unitPrice(BigDecimal.valueOf(24.35))
+                .imageUrl("www.test.com")
+                .active(true)
+                .unitsInStock(10)
+                .dateCreated(LocalDateTime.now())
+                .lastUpdated(LocalDateTime.now())
+                .productCategory(productCategory)
+                .build();
+    }
+}
