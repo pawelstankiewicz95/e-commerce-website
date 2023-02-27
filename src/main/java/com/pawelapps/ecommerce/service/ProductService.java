94ba4e6793de90820682e9cb9529aa71669e4ddd
pawelstankiewicz <pawelsta95@gmail.com>
2023-02-27 23:50:23 +0100

    feat: create ProductSerivce interface

---
+++ src/main/java/com/pawelapps/ecommerce/service/ProductService.java
@@ -0,0 +1,7 @@
+package com.pawelapps.ecommerce.service;
+
+import com.pawelapps.ecommerce.entity.Product;
+
+public interface ProductService {
+    public Product createProduct(Product product);
+}
