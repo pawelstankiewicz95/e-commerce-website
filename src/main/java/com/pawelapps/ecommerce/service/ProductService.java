0f678356bdcdb08e0e9e12cdb19c7e4cb64a9424
pawelstankiewicz <pawelsta95@gmail.com>
2023-03-01 23:53:48 +0100

    feat: add 'deleteProductById' method

--- src/main/java/com/pawelapps/ecommerce/service/ProductService.java
+++ src/main/java/com/pawelapps/ecommerce/service/ProductService.java
@@ -7,7 +7,12 @@ import java.util.List;
 
 public interface ProductService {
     Product createProduct(Product product);
+
     Product getProductById(Long id);
+
     List<Product> getAllProducts();
+
     Product updateProduct(Product product);
+
+    void deleteProductById(Long id);
 }
--- src/main/java/com/pawelapps/ecommerce/service/ProductServiceImpl.java
+++ src/main/java/com/pawelapps/ecommerce/service/ProductServiceImpl.java
@@ -41,4 +41,9 @@ public class ProductServiceImpl implements ProductService {
         return productRepository.save(product);
     }
 
+    @Override
+    public void deleteProductById(Long id) {
+        productRepository.deleteById(id);
+    }
+
 }
