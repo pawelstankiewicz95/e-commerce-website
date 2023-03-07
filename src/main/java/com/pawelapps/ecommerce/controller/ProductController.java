05d59824930b05656589e512a928748d38e70bbc
pawelstankiewicz <pawelsta95@gmail.com>
2023-03-07 22:40:37 +0100

    feat: add '@CrossOrigin' annotation

--- src/main/java/com/pawelapps/ecommerce/controller/ProductController.java
+++ src/main/java/com/pawelapps/ecommerce/controller/ProductController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("http://localhost:4200")
 public class ProductController {
 
     private final ProductService productService;
