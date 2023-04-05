e060e52fc9de57a38e1c05275fe605797bf19a5e
pawelstankiewicz <pawelsta95@gmail.com>
2023-04-06 00:27:45 +0200

    feat: create OrderItem entity

---
+++ src/main/java/com/pawelapps/ecommerce/entity/OrderItem.java
@@ -0,0 +1,37 @@
+package com.pawelapps.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "order_item")
+@Getter
+@Setter
+@NoArgsConstructor
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "unit_price")
+    private int unitPrice;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", referencedColumnName = "id")
+    private Order order;
+}
