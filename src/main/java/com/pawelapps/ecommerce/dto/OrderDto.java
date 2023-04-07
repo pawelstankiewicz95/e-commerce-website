ca5272c49d7f138e94033697da2111f8b4bcc5d2
pawelstankiewicz <pawelsta95@gmail.com>
2023-04-07 23:41:44 +0200

    feat: add lombok annotations

--- src/main/java/com/pawelapps/ecommerce/dto/OrderDto.java
+++ src/main/java/com/pawelapps/ecommerce/dto/OrderDto.java
@@ -4,9 +4,15 @@ import com.pawelapps.ecommerce.entity.Customer;
 import com.pawelapps.ecommerce.entity.OrderItem;
 import com.pawelapps.ecommerce.entity.ShippingAddress;
 import com.pawelapps.ecommerce.entity.Summary;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class OrderDto {
     private Customer customer;
     private ShippingAddress shippingAddress;
