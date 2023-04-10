d4a18141952db0bfdf8d704184490bfdeed39587
pawelstankiewicz <pawelsta95@gmail.com>
2023-04-10 21:14:54 +0200

    feat: add lombok annotations

--- src/main/java/com/pawelapps/ecommerce/entity/Order.java
+++ src/main/java/com/pawelapps/ecommerce/entity/Order.java
@@ -1,9 +1,7 @@
 package com.pawelapps.ecommerce.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -13,6 +11,8 @@ import java.util.Set;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Order {
 
     @Id
