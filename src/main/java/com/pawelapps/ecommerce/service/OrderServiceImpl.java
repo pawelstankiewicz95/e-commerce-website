4eeba23f998fff010f9bf231869bc3721ce144a8
pawelstankiewicz <pawelsta95@gmail.com>
2023-04-10 21:19:41 +0200

    feat: create OrderService implementation

---
+++ src/main/java/com/pawelapps/ecommerce/service/OrderServiceImpl.java
@@ -0,0 +1,42 @@
+package com.pawelapps.ecommerce.service;
+
+import com.pawelapps.ecommerce.dao.OrderRepository;
+import com.pawelapps.ecommerce.dto.OrderDto;
+import com.pawelapps.ecommerce.entity.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Set;
+
+@Service
+@Transactional
+public class OrderServiceImpl implements OrderService {
+
+    private OrderRepository orderRepository;
+
+    @Autowired
+    OrderServiceImpl(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @Override
+    public Order saveOrder(OrderDto orderDto) {
+        Customer customer = orderDto.getCustomer();
+        ShippingAddress shippingAddress = orderDto.getShippingAddress();
+        Summary summary = orderDto.getSummary();
+        Set<OrderItem> orderItems = orderDto.getOrderItems();
+        Order order = Order.builder()
+                .customer(customer)
+                .shippingAddress(shippingAddress)
+                .summary(summary)
+                .orderItems(orderItems).build();
+        return orderRepository.save(order);
+    }
+
+    @Override
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+}
