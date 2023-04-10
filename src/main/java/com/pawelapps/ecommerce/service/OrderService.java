2d7b4296cbabd01d6b99236f3216ae4e1586f815
pawelstankiewicz <pawelsta95@gmail.com>
2023-04-10 21:15:43 +0200

    fix: change saveOrder parameter to orderDto

--- src/main/java/com/pawelapps/ecommerce/service/OrderService.java
+++ src/main/java/com/pawelapps/ecommerce/service/OrderService.java
@@ -1,9 +1,10 @@
 package com.pawelapps.ecommerce.service;
 
+import com.pawelapps.ecommerce.dto.OrderDto;
 import com.pawelapps.ecommerce.entity.Order;
 
 public interface OrderService {
-    Order saveOrder(Order order);
+    Order saveOrder(OrderDto orderDto);
 
     Order getAllOrders();
 }
