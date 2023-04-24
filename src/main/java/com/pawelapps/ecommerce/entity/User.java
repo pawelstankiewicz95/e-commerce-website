4098cf237cec93c587baa2a9be97ac60a551a660
pawelstankiewicz <pawelsta95@gmail.com>
2023-04-24 23:33:37 +0200

    feat: create User entity

---
+++ src/main/java/com/pawelapps/ecommerce/entity/User.java
@@ -0,0 +1,24 @@
+package com.pawelapps.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "email")
+    private String email;
+
+    @OneToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+}
