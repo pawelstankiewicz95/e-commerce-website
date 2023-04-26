8604b2316f0f6c96c5b2214f4bed6eb1f8b8ed0b
pawelstankiewicz <pawelsta95@gmail.com>
2023-04-26 22:57:08 +0200

    feat: create Cart repository

---
+++ src/main/java/com/pawelapps/ecommerce/dao/CartRepository.java
@@ -0,0 +1,7 @@
+package com.pawelapps.ecommerce.dao;
+
+import com.pawelapps.ecommerce.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
