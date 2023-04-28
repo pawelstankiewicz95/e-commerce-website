829e3b27ba916e38e657b40f65897a621d3bf4cc
pawelstankiewicz <pawelsta95@gmail.com>
2023-04-29 00:18:01 +0200

    feat: add variables

--- src/main/java/com/pawelapps/ecommerce/entity/CartProduct.java
+++ src/main/java/com/pawelapps/ecommerce/entity/CartProduct.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.math.BigDecimal;
+
 @Entity
 @Table(name = "cart_product")
 @Getter
@@ -14,11 +16,29 @@ import lombok.*;
 public class CartProduct {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "surrogate_id")
+    private Long surrogateId;
+
+    @Column(name = "cart_product_id")
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "cart_id")
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinColumn(name = "cart_id", referencedColumnName = "id")
     @JsonBackReference
     private Cart cart;
 }
