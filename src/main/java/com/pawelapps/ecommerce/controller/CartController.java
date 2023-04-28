9b85ced6c4b3101d05878fe29e55bc196c49136c
pawelstankiewicz <pawelsta95@gmail.com>
2023-04-29 00:27:22 +0200

    feat: create cart controller

---
+++ src/main/java/com/pawelapps/ecommerce/controller/CartController.java
@@ -0,0 +1,27 @@
+package com.pawelapps.ecommerce.controller;
+
+import com.pawelapps.ecommerce.dto.CartDto;
+import com.pawelapps.ecommerce.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@Controller
+@RequestMapping("/api")
+public class CartController {
+    private CartService cartService;
+
+    @Autowired
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    @PutMapping("/cart")
+    public ResponseEntity<CartDto> saveCart(@RequestBody CartDto cartDto) {
+        cartService.saveCart(cartDto);
+        return new ResponseEntity<>(cartDto, HttpStatus.CREATED);
+    }
+}
