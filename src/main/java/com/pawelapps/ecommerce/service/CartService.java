5210b0eaa05859f8120332d6a2a6af7dcde9202c
pawelstankiewicz <pawelsta95@gmail.com>
2023-05-04 00:40:40 +0200

    feat: add deleteCartByUserEmail method

--- src/main/java/com/pawelapps/ecommerce/service/CartService.java
+++ src/main/java/com/pawelapps/ecommerce/service/CartService.java
@@ -6,4 +6,5 @@ import com.pawelapps.ecommerce.entity.Cart;
 public interface CartService {
     Cart saveCart(CartDto cartDto);
     CartDto getCartByUserEmail(String email);
+    void deleteCartByUserEmail(String email);
 }
