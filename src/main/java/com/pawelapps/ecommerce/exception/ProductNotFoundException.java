cedaf018ff1706a776613f40376033cf93d3a6bb
pawelstankiewicz <pawelsta95@gmail.com>
2023-05-15 23:18:31 +0200

    refactor: make one class NotFoundException for all entities

--- src/main/java/com/pawelapps/ecommerce/exception/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.pawelapps.ecommerce.exception;
-
-public class ProductNotFoundException extends RuntimeException{
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
