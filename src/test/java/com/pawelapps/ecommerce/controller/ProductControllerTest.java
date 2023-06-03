ff1d9d164016463118167ae2d25edc7ddac595ba
pawelstankiewicz <pawelsta95@gmail.com>
2023-06-03 22:14:47 +0200

    refactor: create new tests with mocked service and with security handling

--- src/test/java/com/pawelapps/ecommerce/controller/ProductControllerTest.java
+++ src/test/java/com/pawelapps/ecommerce/controller/ProductControllerTest.java
@@ -1,160 +1,253 @@
 package com.pawelapps.ecommerce.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pawelapps.ecommerce.configuration.SecurityConfiguration;
 import com.pawelapps.ecommerce.entity.Product;
-import com.pawelapps.ecommerce.entity.ProductCategory;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import org.junit.jupiter.api.BeforeEach;
+import com.pawelapps.ecommerce.service.ProductService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@TestPropertySource("/test-application.properties")
+@WebMvcTest(ProductController.class)
+@Import(SecurityConfiguration.class)
 @AutoConfigureMockMvc
-@Transactional
 public class ProductControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
-    @PersistenceContext
-    EntityManager entityManager;
+    @MockBean
+    ProductService productService;
 
     @Autowired
     ObjectMapper objectMapper;
 
     Product product;
 
-    ProductCategory productCategory;
-
-    @BeforeEach
-    void setUpDataBase() {
-
-        productCategory = ProductCategory.builder().categoryName("Cup").build();
-        entityManager.persist(productCategory);
-
-        product = Product.builder()
-                .sku("123456")
-                .name("TestCup")
-                .description("Just testing cup")
-                .unitPrice(BigDecimal.valueOf(24.35))
-                .imageUrl("www.test.com")
-                .active(true)
-                .unitsInStock(10)
-                .dateCreated(LocalDateTime.now())
-                .lastUpdated(LocalDateTime.now())
-                .productCategory(productCategory)
-                .build();
-        entityManager.persist(product);
-
-        product = Product.builder()
-                .sku("888999")
-                .name("Second TestCup")
-                .description("Just second testing cup")
-                .unitPrice(BigDecimal.valueOf(55.45))
-                .imageUrl("www.secondimage.com")
-                .active(true)
-                .unitsInStock(10)
-                .dateCreated(LocalDateTime.now())
-                .lastUpdated(LocalDateTime.now())
-                .productCategory(productCategory)
-                .build();
-        entityManager.persist(product);
-
-        entityManager.flush();
-
-    }
 
     @Test
+    @DisplayName("when getting all products")
     void getAllProductsTest() throws Exception {
+
+        List<Product> productList = Arrays.asList(
+                Product.builder()
+                        .sku("123")
+                        .name("Product 1")
+                        .description("Description 1")
+                        .build(),
+                Product.builder()
+                        .sku("456")
+                        .name("Product 2")
+                        .description("Description 2")
+                        .build()
+        );
+
+        when(productService.getAllProducts()).thenReturn(productList);
+
         mockMvc.perform(MockMvcRequestBuilders.get("/api/products"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$", hasSize(2)));
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].name", is("Product 1")))
+                .andExpect(jsonPath("$[0].description", is("Description 1")))
+                .andExpect(jsonPath("$[1].name", is("Product 2")))
+                .andExpect(jsonPath("$[1].description", is("Description 2")));
+
+        verify(productService, times(1)).getAllProducts();
     }
-
-    @Test
-    void createProductTest() throws Exception {
-        product = Product.builder()
-                .sku("333222")
-                .name("New TestCup")
-                .description("Just new testing cup")
-                .unitPrice(BigDecimal.valueOf(15.45))
-                .imageUrl("www.newimage.com")
-                .active(true)
-                .unitsInStock(10)
-                .dateCreated(LocalDateTime.now())
-                .lastUpdated(LocalDateTime.now())
-                .productCategory(productCategory)
-                .build();
-
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/products")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(product)))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath(("$.sku"), is("333222")));
-    }
-
-    @Test
-    void updateProductTest() throws Exception {
-        product = Product.builder()
-                .id(1L)
-                .sku("333222")
-                .name("Updated Name")
-                .description("Just new updated cup")
-                .unitPrice(BigDecimal.valueOf(15.45))
-                .imageUrl("www.updatedimage.com")
-                .active(true)
-                .unitsInStock(10)
-                .dateCreated(LocalDateTime.now())
-                .lastUpdated(LocalDateTime.now())
-                .productCategory(productCategory)
-                .build();
-
-        mockMvc.perform(MockMvcRequestBuilders.put("/api/products")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(product)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name", is("Updated Name")));
-    }
-
 
     @Nested
-    @DisplayName("getProductsByNameLikeOrSkuLike method")
-    class getProductsByNameOrSkuTest {
+    @DisplayName("When creating product")
+    class CreateProductTest {
+
         @Test
-        @DisplayName("when getting by name like")
-        void getProductsByName() throws Exception {
-            mockMvc.perform(MockMvcRequestBuilders.get("/api/products/products-by-name-or-sku/Second"))
-                    .andExpect(status().isOk())
-                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(jsonPath("$", hasSize(1)))
-                    .andExpect(jsonPath("$[0].name", is("Second TestCup")));
+        @DisplayName("When anonymous wants to access")
+        void createProductAsAnonymous() throws Exception {
+            product = Product.builder()
+                    .sku("333222")
+                    .name("New TestCup")
+                    .description("Just new testing cup")
+                    .build();
+
+            when(productService.createProduct(any(Product.class))).thenReturn(product);
+
+            mockMvc.perform(MockMvcRequestBuilders.post("/api/products")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(product)))
+                    .andExpect(status().isForbidden());
+
+            verify(productService, times(0)).updateProduct(any(Product.class));
         }
+
         @Test
-        @DisplayName("when getting by sku like")
-        void getProductsBySku() throws Exception {
-            mockMvc.perform(MockMvcRequestBuilders.get("/api/products/products-by-name-or-sku/34"))
-                    .andExpect(status().isOk())
-                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(jsonPath("$", hasSize(1)))
-                    .andExpect(jsonPath("$[0].name", is("TestCup")));
+        @DisplayName("When unauthorized user wants to access")
+        @WithMockUser(authorities = "user")
+        void createProductAsUnauthorizedUser() throws Exception {
+            product = Product.builder()
+                    .sku("333222")
+                    .name("New TestCup")
+                    .description("Just new testing cup")
+                    .build();
+
+            when(productService.createProduct(any(Product.class))).thenReturn(product);
+
+            mockMvc.perform(MockMvcRequestBuilders.post("/api/products")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(product)))
+                    .andExpect(status().isForbidden());
+
+            verify(productService, times(0)).updateProduct(any(Product.class));
         }
+
+        @Test
+        @DisplayName("When authorized user wants to access")
+        @WithMockUser(authorities = "admin")
+        void createProductAsAuthorizedUser() throws Exception {
+            product = Product.builder()
+                    .sku("333222")
+                    .name("New TestCup")
+                    .description("Just new testing cup")
+                    .build();
+
+            when(productService.createProduct(any(Product.class))).thenReturn(product);
+
+            mockMvc.perform(MockMvcRequestBuilders.post("/api/products")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(product)))
+                    .andExpect(status().isCreated())
+                    .andExpect(jsonPath(("$.sku"), is("333222")));
+
+            verify(productService, times(1)).createProduct(any(Product.class));
+
+        }
+    }
+
+    @Nested
+    @DisplayName("When updating product")
+    class UpdateProductTests {
+
+        @Test
+        @DisplayName("When anonymous user wants to access")
+        void updateProductTestAsAnonymous() throws Exception {
+            product = Product.builder()
+                    .id(123L)
+                    .sku("333222")
+                    .name("Product 1")
+                    .description("Description 1")
+                    .build();
+
+            when(productService.updateProduct(any(Product.class))).thenReturn(product);
+
+            mockMvc.perform(MockMvcRequestBuilders.put("/api/products")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(product)))
+                    .andExpect(status().isForbidden());
+            verify(productService, times(0)).updateProduct(any(Product.class));
+        }
+
+        @Test
+        @DisplayName("When unauthorized user wants to access")
+        @WithMockUser(authorities = "user")
+        void updateProductTestAsUnauthorized() throws Exception {
+            product = Product.builder()
+                    .id(123L)
+                    .sku("333222")
+                    .name("Product 1")
+                    .description("Description 1")
+                    .build();
+
+            when(productService.updateProduct(any(Product.class))).thenReturn(product);
+
+            mockMvc.perform(MockMvcRequestBuilders.put("/api/products")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(product)))
+                    .andExpect(status().isForbidden());
+            verify(productService, times(0)).updateProduct(any(Product.class));
+        }
+
+        @Test
+        @DisplayName("When authorized user wants to access")
+        @WithMockUser(authorities = "admin")
+        void updateProductTestAsAuthorized() throws Exception {
+            product = Product.builder()
+                    .id(123L)
+                    .sku("333222")
+                    .name("Product 1")
+                    .description("Description 1")
+                    .build();
+
+            when(productService.updateProduct(any(Product.class))).thenReturn(product);
+
+            mockMvc.perform(MockMvcRequestBuilders.put("/api/products")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(product)))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.id", is(123)))
+                    .andExpect(jsonPath("$.sku", is("333222")))
+                    .andExpect(jsonPath("$.name", is("Product 1")))
+                    .andExpect(jsonPath("$.description", is("Description 1")));
+            verify(productService, times(1)).updateProduct(any(Product.class));
+        }
+    }
+
+    @Test
+    @DisplayName("when getting by name or sku")
+    void getProductsByNameOrSkuTest() throws Exception {
+        List<Product> testProducts = Arrays.asList(
+                Product.builder().id(1L).name("Product 1").sku("123").build(),
+                Product.builder().id(2L).name("Product 2").sku("456").build()
+        );
+
+        when(productService.getProductsByNameOrSku("Second")).thenReturn(testProducts);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/products/products-by-name-or-sku/{nameOrSku}", "Second"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].name", is("Product 1")))
+                .andExpect(jsonPath("$[1].name", is("Product 2")));
+
+        verify(productService, times(1)).getProductsByNameOrSku("Second");
+    }
+
+    @Test
+    void getProductByIdTest() throws Exception {
+        product = Product.builder()
+                .id(123L)
+                .sku("123")
+                .name("Product 1")
+                .description("Description 1")
+                .build();
+
+        when(productService.getProductById(123L)).thenReturn(product);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/products/{id}", 123))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                //  .andExpect(jsonPath("$.id", 123))
+                .andExpect(jsonPath("$.sku", is("123")))
+                .andExpect(jsonPath("$.name", is("Product 1")))
+                .andExpect(jsonPath("$.description", is("Description 1")));
+
+        verify(productService, times(1)).getProductById(123L);
+
     }
 }
