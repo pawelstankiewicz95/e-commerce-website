342ef711f35c620facd15e153de0e50f34c1a462
pawelstankiewicz <pawelsta95@gmail.com>
2023-06-22 22:37:37 +0200

    feature: make findByNameLikeOrSkuLike method case insensitive

--- src/main/java/com/pawelapps/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/pawelapps/ecommerce/dao/ProductRepository.java
@@ -11,6 +11,6 @@ public interface ProductRepository extends JpaRepository<Product,Long> {
     List<Product> findByName(String name);
     List<Product> findByProductCategoryId(Long id);
 
-    @Query("SELECT p FROM Product p WHERE p.name LIKE %:nameOrSku% OR p.sku LIKE %:nameOrSku%")
+    @Query("SELECT p FROM Product p WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :nameOrSku, '%')) OR LOWER(p.sku) LIKE LOWER(CONCAT('%', :nameOrSku, '%'))")
     List<Product> findByNameLikeOrSkuLike(@Param("nameOrSku") String nameOrSku);
 }
