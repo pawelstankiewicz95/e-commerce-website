f2359f7177335ba48e998ab870985640acf4de3a
pawelstankiewicz <pawelsta95@gmail.com>
2023-06-22 22:25:19 +0200

    test/refactor: refactor methods and variables

--- src/it/java/com/pawelapps/ecommerce/dao/ProductRepositoryIT.java
+++ src/it/java/com/pawelapps/ecommerce/dao/ProductRepositoryIT.java
@@ -3,26 +3,55 @@ package com.pawelapps.ecommerce.dao;
 import com.pawelapps.ecommerce.BaseIT;
 import com.pawelapps.ecommerce.entity.Product;
 import com.pawelapps.ecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Transactional
 class ProductRepositoryIT extends BaseIT {
 
     @Autowired
     ProductRepository productRepository;
 
     @Autowired
-    static ProductCategoryRepository productCategoryRepository;
-    static ProductCategory productCategory;
+    EntityManager entityManager;
 
-    @BeforeAll
-    static void setUp(){
+    @Autowired
+    ProductCategoryRepository productCategoryRepository;
+    ProductCategory productCategory;
+
+    Product product1;
+    Product product2;
+
+    @BeforeEach
+    void setUp() {
         productCategory = productCategoryRepository.save(ProductCategory.builder().categoryName("Test Category 1").build());
+        product1 = productRepository.save(Product.builder()
+                .sku("PRODUCT1")
+                .name("Test Product 1")
+                .productCategory(productCategory)
+                .unitsInStock(3)
+                .active(true)
+                .build());
+
+        product2 = productRepository.save(Product.builder()
+                .sku("PRODUCT2")
+                .name("Test Product 2")
+                .productCategory(productCategory)
+                .unitsInStock(4)
+                .active(false)
+                .build());
+
+        entityManager.persist(productCategory);
+        entityManager.persist(product1);
+        entityManager.persist(product2);
+        entityManager.flush();
     }
 
     @Test
@@ -34,6 +63,8 @@ class ProductRepositoryIT extends BaseIT {
                 .unitsInStock(3)
                 .active(true)
                 .build());
+        entityManager.persist(savedProduct);
+        entityManager.flush();
 
         assertNotNull(savedProduct.getId(), "Id should not be null");
         assertEquals("TEST1", savedProduct.getSku(), "Product SKU should match");
@@ -42,68 +73,60 @@ class ProductRepositoryIT extends BaseIT {
     }
 
     @Test
-    void shouldGetProduct() {
+    void shouldFindProductById() {
 
-        Product savedProduct = productRepository.save(Product.builder()
-                .sku("TEST2")
-                .name("Test Product 2")
-                .productCategory(productCategory)
-                .unitsInStock(3)
-                .active(true)
-                .build());
-
-        Product productFromDatabase = productRepository.findById(savedProduct.getId()).orElse(null);
+        Product productFromDatabase = productRepository.findById(product1.getId()).orElse(null);
 
         assertNotNull(productFromDatabase, "productFromDatabase should not be null");
-        assertEquals("TEST2", productFromDatabase.getSku(), "Product SKU should match");
-        assertEquals("TEST2", productFromDatabase.getName(), "Product name should match");
+        assertEquals("PRODUCT1", productFromDatabase.getSku(), "Product SKU should match");
+        assertEquals("Test Product 1", productFromDatabase.getName(), "Product name should match");
     }
 
     @Test
     void shouldGetAllProducts() {
-        Product savedProduct1 = productRepository.save(Product.builder()
-                .sku("TEST3")
-                .name("Test Product 3")
-                .productCategory(productCategory)
-                .unitsInStock(3)
-                .active(true)
-                .build());
-        Product savedProduct2 = productRepository.save(Product.builder()
-                .sku("TEST4")
-                .name("Test Product 4")
-                .productCategory(productCategory)
-                .unitsInStock(4)
-                .active(false)
-                .build());
+
 
         List<Product> products = productRepository.findAll();
 
         assertFalse(products.isEmpty(), "List should not be empty");
         assertTrue(products.size() > 1, "List should have at least two products");
-        assertNotNull(products.stream().filter(p -> p.getId().equals(savedProduct1.getId())).findFirst(),"Should find savedProduct1 by id");
+        assertNotNull(products.stream().filter(p -> p.getId().equals(product1.getId())).findFirst(), "Should find product1 by id");
+        assertNotNull(products.stream().filter(p -> p.getId().equals(product2.getId())).findFirst(), "Should find product2 by id");
     }
 
     @Test
     void updateProductTest() {
-        Product productFromDataBase = productRepository.findById(1L).orElse(null);
+
+        Product productFromDataBase = productRepository.findById(product1.getId()).orElse(null);
         assertNotNull(productFromDataBase, "Object should not be null");
-        productFromDataBase.setName("updated Cup");
+        productFromDataBase.setName("Updated product 1");
         Product updatedProduct = productRepository.save(productFromDataBase);
 
         assertEquals(productFromDataBase.getId(), updatedProduct.getId(), "Id's should be the same");
-        assertEquals("updated Cup", updatedProduct.getName(), "Product names should be the same");
+        assertEquals("Updated product 1", updatedProduct.getName(), "Product names should be the same");
+
     }
 
     @Test
     void deleteProductTest() {
-        Product productFromDataBase = productRepository.findById(1L).orElse(null);
+        Product savedProduct = productRepository.save(Product.builder()
+                .sku("DELETE_TEST1")
+                .name("Delete test Product")
+                .productCategory(productCategory)
+                .unitsInStock(3)
+                .active(true)
+                .build());
+        entityManager.persist(savedProduct);
+        entityManager.flush();
+
+        Product productFromDataBase = productRepository.findById(savedProduct.getId()).orElse(null);
 
         assertNotNull(productFromDataBase, "productFromDatabase should not be null");
 
         productRepository.delete(productFromDataBase);
 
-        Optional<Product> optionalProduct = productRepository.findById(1L);
-        Optional<ProductCategory> optionalProductCategory = productCategoryRepository.findById(1L);
+        Optional<Product> optionalProduct = productRepository.findById(savedProduct.getId());
+        Optional<ProductCategory> optionalProductCategory = productCategoryRepository.findById(productCategory.getId());
 
         assertTrue(optionalProduct.isEmpty(), "Optional product should be empty");
         assertFalse(optionalProductCategory.isEmpty(), "Product category should not be deleted");
@@ -112,25 +135,29 @@ class ProductRepositoryIT extends BaseIT {
 
     @Test
     void findByName() {
-        List<Product> products = productRepository.findByName("TestCup");
+        List<Product> products = productRepository.findByName("Test Product 1");
+
         assertFalse(products.isEmpty(), "list should not be empty");
     }
 
     @Nested
-    @DisplayName("findByNameLikeOrSkuLike method")
     class findByNameLikeOrSkuLikeTest {
         @Test
-        @DisplayName("when finding by name")
-        void findByNameLikeTest() {
-            List<Product> products = productRepository.findByNameLikeOrSkuLike("est");
-            assertFalse(products.isEmpty(), "list should not be empty");
+        void shouldFindProductByIncompleteName() {
+            List<Product> productsFoundByUpperCaseName = productRepository.findByNameLikeOrSkuLike("EST");
+            List<Product> productsFoundByLowerCaseName = productRepository.findByNameLikeOrSkuLike("est");
+
+            assertFalse(productsFoundByUpperCaseName.isEmpty(), "list should not be empty");
+            assertFalse(productsFoundByLowerCaseName.isEmpty(), "list should not be empty");
         }
 
         @Test
-        @DisplayName("when finding by sku")
-        void findBySkuLikeTest() {
-            List<Product> products = productRepository.findByNameLikeOrSkuLike("45");
-            assertFalse(products.isEmpty(), "list should not be empty");
+        void shouldFindProductByIncompleteSku() {
+            List<Product> productsFoundByUpperCaseSku = productRepository.findByNameLikeOrSkuLike("UCT1");
+            List<Product> productsFoundByLowerCaseSku = productRepository.findByNameLikeOrSkuLike("uct1");
+
+            assertFalse(productsFoundByUpperCaseSku.isEmpty(), "list should not be empty");
+            assertFalse(productsFoundByLowerCaseSku.isEmpty(), "list should not be empty");
         }
     }
 }
