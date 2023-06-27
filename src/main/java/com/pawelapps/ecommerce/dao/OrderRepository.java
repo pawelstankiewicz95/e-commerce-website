1f221ccfaf187b6258fcd0fe3ee5e99a49d0071d
pawelstankiewicz <pawelsta95@gmail.com>
2023-06-27 22:54:09 +0200

    feat: add findByCustomerEmail method

--- src/main/java/com/pawelapps/ecommerce/dao/OrderRepository.java
+++ src/main/java/com/pawelapps/ecommerce/dao/OrderRepository.java
@@ -2,6 +2,12 @@ package com.pawelapps.ecommerce.dao;
 
 import com.pawelapps.ecommerce.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findByCustomerEmail(String email);
+
 }
