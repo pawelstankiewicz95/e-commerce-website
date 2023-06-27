1b4a3e1921a47fc142d5d0b8618a48ce68c5700a
pawelstankiewicz <pawelsta95@gmail.com>
2023-06-27 22:53:29 +0200

    feat: add endpoint for getting orders by email

--- src/main/java/com/pawelapps/ecommerce/controller/OrderController.java
+++ src/main/java/com/pawelapps/ecommerce/controller/OrderController.java
@@ -3,14 +3,16 @@ package com.pawelapps.ecommerce.controller;
 import com.pawelapps.ecommerce.dto.OrderDto;
 import com.pawelapps.ecommerce.entity.Order;
 import com.pawelapps.ecommerce.service.OrderService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/api")
@@ -18,13 +20,19 @@ public class OrderController {
     private final OrderService orderService;
 
     @Autowired
-    public OrderController(OrderService orderService){
+    public OrderController(OrderService orderService) {
         this.orderService = orderService;
     }
 
     @PostMapping("/purchase")
-    public ResponseEntity<OrderDto> saveOrder(@RequestBody OrderDto orderDto){
+    public ResponseEntity<OrderDto> saveOrder(@RequestBody OrderDto orderDto) {
         orderService.saveOrder(orderDto);
         return new ResponseEntity<>(orderDto, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/orders/{customerEmail}")
+    public ResponseEntity<List<OrderDto>> findOrdersByCustomerEmail(@PathVariable("customerEmail") String customerEmail) throws AccessDeniedException {
+        List<OrderDto> orderDtos = orderService.findByCustomerEmail(customerEmail);
+        return new ResponseEntity<>(orderDtos, HttpStatus.OK);
     }
 }
