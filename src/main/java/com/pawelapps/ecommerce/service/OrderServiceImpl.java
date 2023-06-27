88d114ab35ab75b4d77a4377265f387f12b5b0f3
pawelstankiewicz <pawelsta95@gmail.com>
2023-06-27 22:55:10 +0200

    feat: add findByCustomerEmail method / refactor with modelMapper

--- src/main/java/com/pawelapps/ecommerce/service/OrderServiceImpl.java
+++ src/main/java/com/pawelapps/ecommerce/service/OrderServiceImpl.java
@@ -3,40 +3,47 @@ package com.pawelapps.ecommerce.service;
 import com.pawelapps.ecommerce.dao.OrderRepository;
 import com.pawelapps.ecommerce.dto.OrderDto;
 import com.pawelapps.ecommerce.entity.*;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class OrderServiceImpl implements OrderService {
 
     private OrderRepository orderRepository;
+    private ModelMapper modelMapper;
 
     @Autowired
-    OrderServiceImpl(OrderRepository orderRepository) {
+    OrderServiceImpl(OrderRepository orderRepository, ModelMapper modelMapper) {
         this.orderRepository = orderRepository;
+        this.modelMapper = modelMapper;
+
     }
 
     @Override
     public Order saveOrder(OrderDto orderDto) {
-        Customer customer = orderDto.getCustomer();
-        ShippingAddress shippingAddress = orderDto.getShippingAddress();
-        Summary summary = orderDto.getSummary();
-        Set<OrderProduct> orderProducts = orderDto.getOrderProducts();
-        Order order = Order.builder()
-                .customer(customer)
-                .shippingAddress(shippingAddress)
-                .summary(summary).build();
-        orderProducts.forEach(product -> order.addOrderProduct(product));
+        Order order = modelMapper.map(orderDto, Order.class);
         return orderRepository.save(order);
     }
+
+
 
     @Override
     public List<Order> getAllOrders() {
         return orderRepository.findAll();
     }
+
+    @Override
+    public List<OrderDto> findByCustomerEmail(String email) {
+        List<Order> orders = orderRepository.findByCustomerEmail(email);
+        List<OrderDto> ordersDto = orders.stream().map(o -> modelMapper.map(o, OrderDto.class)).collect(Collectors.toList());
+        return ordersDto;
+    }
+
 }
