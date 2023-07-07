5d76e16d04b41ff028f58cbe73b4bc2edffa9db6
pawelstankiewicz <pawelsta95@gmail.com>
2023-07-07 22:23:53 +0200

    feat: add findOrderById method/ change @PathVariable with @RequestParam

--- src/main/java/com/pawelapps/ecommerce/controller/OrderController.java
+++ src/main/java/com/pawelapps/ecommerce/controller/OrderController.java
@@ -1,9 +1,7 @@
 package com.pawelapps.ecommerce.controller;
 
 import com.pawelapps.ecommerce.dto.OrderDto;
-import com.pawelapps.ecommerce.entity.Order;
 import com.pawelapps.ecommerce.service.OrderService;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/api")
@@ -39,16 +36,23 @@ public class OrderController {
     }
 
     @PreAuthorize("hasAuthority('admin')")
-    @GetMapping("/orders/{customerEmail}")
-    public ResponseEntity<List<OrderDto>> findOrdersByCustomerEmail(@PathVariable("customerEmail") String customerEmail) throws AccessDeniedException {
+    @GetMapping("/orders/customer")
+    public ResponseEntity<List<OrderDto>> findOrdersByCustomerEmail(@RequestParam("customerEmail") String customerEmail) throws AccessDeniedException {
         List<OrderDto> orderDtos = orderService.findByCustomerEmail(customerEmail);
         return new ResponseEntity<>(orderDtos, HttpStatus.OK);
     }
 
     @PreAuthorize("hasAuthority('admin') or #principal?.userName == #userEmail")
-    @GetMapping("/orders/{userEmail}")
-    public ResponseEntity<List<OrderDto>> findOrdersByUserEmail(@PathVariable("userEmail") String userEmail, Principal principal) throws AccessDeniedException {
+    @GetMapping("/orders/user")
+    public ResponseEntity<List<OrderDto>> findOrdersByUserEmail(@RequestParam("userEmail") String userEmail, Principal principal) throws AccessDeniedException {
         List<OrderDto> ordersDto = orderService.findByUserEmail(userEmail);
         return new ResponseEntity<>(ordersDto, HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasAuthority('admin') or #principal?.userName == #userEmail")
+    @GetMapping("/orders/id")
+    public ResponseEntity<OrderDto> findOrderById(@RequestParam("id") Long id, Principal principal) {
+        OrderDto orderDto = this.orderService.findById(id);
+        return new ResponseEntity<>(orderDto, HttpStatus.OK);
     }
 }
