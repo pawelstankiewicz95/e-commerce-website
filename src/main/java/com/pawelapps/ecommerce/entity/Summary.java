b49a013e1b9dadabcc1ecf1b098d0cfa2b27cf39
pawelstankiewicz <pawelsta95@gmail.com>
2023-07-23 22:17:06 +0200

    feat: add shippingPrice variable

--- src/main/java/com/pawelapps/ecommerce/entity/Summary.java
+++ src/main/java/com/pawelapps/ecommerce/entity/Summary.java
@@ -19,11 +19,14 @@ public class Summary {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "total_price")
+    @Column(name = "total_cart_value")
     private BigDecimal totalCartValue;
 
     @Column(name = "total_quantity")
     private int totalQuantityOfProducts;
+
+    @Column(name = "shipping_price")
+    private BigDecimal shippingPrice;
 
     @OneToOne(mappedBy = "summary")
     @JsonIgnoreProperties("summary")
