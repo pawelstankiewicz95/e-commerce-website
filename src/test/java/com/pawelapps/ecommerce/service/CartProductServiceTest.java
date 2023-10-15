08e54882065fd439ec411b8d3992fb67be3f5468
pawelstankiewicz <pawelsta95@gmail.com>
2023-10-15 20:50:46 +0200

    test: add IncreaseCartProductQuantityByOne tests

--- src/test/java/com/pawelapps/ecommerce/service/CartProductServiceTest.java
+++ src/test/java/com/pawelapps/ecommerce/service/CartProductServiceTest.java
@@ -2,20 +2,23 @@ package com.pawelapps.ecommerce.service;
 
 import com.pawelapps.ecommerce.dao.CartProductRepository;
 import com.pawelapps.ecommerce.dao.CartRepository;
+import com.pawelapps.ecommerce.dao.ProductRepository;
 import com.pawelapps.ecommerce.dto.CartProductDto;
 import com.pawelapps.ecommerce.entity.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.math.BigDecimal;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class CartProductServiceTest {
@@ -25,6 +28,9 @@ public class CartProductServiceTest {
 
     @MockBean
     private CartRepository cartRepository;
+
+    @MockBean
+    private ProductRepository productRepository;
 
     @Autowired
     private CartProductService cartProductService;
@@ -44,7 +50,7 @@ public class CartProductServiceTest {
     private CartProductDto cartProductDto;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         user = User.builder().email(userEmail).build();
 
         cart = Cart.builder().user(user).build();
@@ -60,6 +66,7 @@ public class CartProductServiceTest {
                 .unitPrice(BigDecimal.valueOf(1)).build();
 
         cartProductDto = CartProductDto.builder()
+                .cartProductId(1L)
                 .product(product)
                 .cartProductId(product.getId())
                 .name(product.getName())
@@ -68,6 +75,7 @@ public class CartProductServiceTest {
                 .build();
 
         cartProduct = CartProduct.builder()
+                .cartProductId(cartProductDto.getCartProductId())
                 .product(product)
                 .cartProductId(cartProductDto.getCartProductId())
                 .name(cartProductDto.getName())
@@ -76,33 +84,66 @@ public class CartProductServiceTest {
                 .build();
     }
 
-    @Test
-    void shouldSaveCartProductWhenCartIsPresent() {
-        when(cartRepository.findByUserEmail(userEmail)).thenReturn(cart);
-        when(cartProductRepository.save(any(CartProduct.class))).thenReturn(cartProduct);
+    @Nested
+    class SaveCartProductTests {
 
-        CartProductDto savedCartProductDto = cartProductService.saveCartProductToCart(cartProductDto, userEmail);
+        @Test
+        void shouldSaveCartProductWhenCartIsPresent() {
+            when(cartRepository.findByUserEmail(userEmail)).thenReturn(cart);
+            when(cartProductRepository.save(any(CartProduct.class))).thenReturn(cartProduct);
 
-        assertNotNull(savedCartProductDto);
-        assertNotNull(savedCartProductDto.getCartProductId());
-        assertEquals(cart, savedCartProductDto.getCart());
+            CartProductDto savedCartProductDto = cartProductService.saveCartProductToCart(cartProductDto, userEmail);
 
-        verify(cartRepository).findByUserEmail(userEmail);
-        verify(cartProductRepository).save(any(CartProduct.class));
+            assertNotNull(savedCartProductDto);
+            assertNotNull(savedCartProductDto.getCartProductId());
+            assertEquals(cart, savedCartProductDto.getCart());
+
+            verify(cartRepository).findByUserEmail(userEmail);
+            verify(cartProductRepository).save(any(CartProduct.class));
+        }
+
+        @Test
+        void shouldSaveCartProductWhenCartIsNotPresent() {
+            when(cartRepository.findByUserEmail(userEmail)).thenReturn(null);
+            when(cartProductRepository.save(any(CartProduct.class))).thenReturn(cartProduct);
+
+            CartProductDto savedCartProductDto = cartProductService.saveCartProductToCart(cartProductDto, userEmail);
+
+            assertNotNull(savedCartProductDto);
+            assertNotNull(savedCartProductDto.getCartProductId());
+            assertNotNull(savedCartProductDto.getCart());
+
+            verify(cartRepository).findByUserEmail(userEmail);
+            verify(cartProductRepository).save(any(CartProduct.class));
+        }
     }
 
-    @Test
-    void shouldSaveCartProductWhenCartIsNotPresent() {
-        when(cartRepository.findByUserEmail(userEmail)).thenReturn(null);
-        when(cartProductRepository.save(any(CartProduct.class))).thenReturn(cartProduct);
+    @Nested
+    class IncreaseCartProductQuantityByOneTests {
 
-        CartProductDto savedCartProductDto = cartProductService.saveCartProductToCart(cartProductDto, userEmail);
+        @Test
+        void shouldIncreaseQuantityWhenProductHasEnoughUnitsInStock() {
+            product.setUnitsInStock(10);
+            cartProduct.setQuantity(9);
+            when(cartProductRepository.findById(cartProduct.getCartProductId())).thenReturn(Optional.ofNullable(cartProduct));
+            when(productRepository.findById(cartProduct.getProduct().getId())).thenReturn(Optional.ofNullable(product));
+            when(cartProductRepository.increaseCartProductQuantityByOne(cartProduct.getCartProductId())).thenReturn(1);
 
-        assertNotNull(savedCartProductDto);
-        assertNotNull(savedCartProductDto.getCartProductId());
-        assertNotNull(savedCartProductDto.getCart());
+            Integer updatedRows = cartProductService.increaseCartProductQuantityByOne(cartProduct.getCartProductId());
 
-        verify(cartRepository).findByUserEmail(userEmail);
-        verify(cartProductRepository).save(any(CartProduct.class));
+            assertEquals(1, updatedRows);
+        }
+
+        @Test
+        void shouldThrowExceptionWhenProductHasNotGotEnoughUnitsInStock() {
+            product.setUnitsInStock(10);
+            cartProduct.setQuantity(10);
+            when(cartProductRepository.findById(cartProduct.getCartProductId())).thenReturn(Optional.ofNullable(cartProduct));
+            when(productRepository.findById(cartProduct.getProduct().getId())).thenReturn(Optional.ofNullable(product));
+
+            assertThrows(IllegalStateException.class, () -> cartProductService.increaseCartProductQuantityByOne(cartProduct.getCartProductId()));
+        }
     }
+
+
 }
